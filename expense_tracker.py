f50d6f54dfe21e599762e5ec9a4557ea44da6fb1
AnantSinghTanwar-ux <anantsinghtanwar@gmail.com>
2026-01-09 14:35:19 +0530

    Add Expense Manager project and update expense_tracker.py

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,6 +1,33 @@
+import csv
 from datetime import datetime
 
-expenses = []
+FILE_NAME = "expenses.csv"
+
+def add_expense():
+    amount = float(input("Enter amount: "))
+    category = input("Enter category: ")
+    date = datetime.now().strftime("%Y-%m-%d")
+
+    with open(FILE_NAME, "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([date, category, amount])
+
+    print("Expense saved successfully!")
+
+def view_expenses():
+    total = 0
+    print("\n--- Expenses ---")
+
+    try:
+        with open(FILE_NAME, "r") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                print(f"{row[0]} | {row[1]} | ₹{row[2]}")
+                total += float(row[2])
+    except FileNotFoundError:
+        print("No expenses recorded yet.")
+
+    print("Total Expense:", total)
 
 while True:
     print("\n1. Add Expense")
@@ -10,30 +37,13 @@ while True:
     choice = input("Choose an option: ")
 
     if choice == "1":
-        amount = float(input("Enter amount: "))
-        category = input("Enter category: ")
-        date = datetime.now().strftime("%Y-%m-%d")
-
-        expense = {
-            "amount": amount,
-            "category": category,
-            "date": date
-        }
-
-        expenses.append(expense)
-        print("Expense added successfully!")
+        add_expense()
 
     elif choice == "2":
-        total = 0
-        print("\n--- Expenses ---")
-        for e in expenses:
-            print(f"{e['date']} | {e['category']} | ₹{e['amount']}")
-            total += e["amount"]
-
-        print("Total Expense:", total)
+        view_expenses()
 
     elif choice == "3":
-        print("Exiting...")
+        print("Goodbye!")
         break
 
     else:
